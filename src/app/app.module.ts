721512fd27399447aa615e9216899bbd1dd4da29
jsmorrugares1 <jsmorrugares@gmail.com>
2019-07-02 16:30:42 -0500

    primer Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,9 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import {Flashlight} from '@ionic-native/flashlight/ngx';
+
+
 
 @NgModule({
   declarations: [AppComponent],
@@ -16,6 +19,7 @@ import { AppRoutingModule } from './app-routing.module';
   providers: [
     StatusBar,
     SplashScreen,
+    Flashlight,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Flashlight} from '@ionic-native/flashlight/ngx';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,20 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private flash: Flashlight) {}
 
+  switchFlash( event)
+  {
+    let power:boolean = event.target.checked;
+    if(power)
+    {
+      this.flash.switchOn();
+      console.log(power);
+    }
+    else
+    {
+      this.flash.switchOff();
+      console.log(power);
+    }
+  }
 }
